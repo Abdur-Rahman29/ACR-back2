37da951ed6b4eb2331ea4022dac1751e98ebcc2f
Chetan4458 <111554115+Chetan4458@users.noreply.github.com>
2024-12-11 12:59:58 +0530

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+
+if __name__ == "__main__":
+    # Get the absolute path to the ReviewBot directory
+    reviewbot_dir = os.path.join(os.getcwd(), "ReviewBot")  # Get the full path to the ReviewBot directory
+    
+    # Check if the directory exists
+    if os.path.isdir(reviewbot_dir):
+        # Run the Django development server in the ReviewBot directory
+        subprocess.run(["python", "manage.py", "runserver","0.0.0.0:8080"], cwd=reviewbot_dir)
+    else:
+        print(f"Error: The directory '{reviewbot_dir}' does not exist.")
