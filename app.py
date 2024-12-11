fbdf5b313b5de66a9977e36e3a81d624ad42068d
Chetan4458 <111554115+Chetan4458@users.noreply.github.com>
2024-12-11 16:30:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     # Check if the directory exists
     if os.path.isdir(reviewbot_dir):
         # Run the Django development server in the ReviewBot directory
-        subprocess.run(["python", "manage.py", "runserver","127.0.0.1:8080"], cwd=reviewbot_dir)
+        subprocess.run(["python", "manage.py", "runserver","0.0.0.0:8080"], cwd=reviewbot_dir)
     else:
         print(f"Error: The directory '{reviewbot_dir}' does not exist.")
